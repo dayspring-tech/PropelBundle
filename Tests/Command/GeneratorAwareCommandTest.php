c7a5560bf6695b51683a2ae64ce961be50801d1d
Jeffrey Wong <jwong@dayspringpartners.com>
2022-12-19 19:38:14 -0800

    fix direct linking of propel1 files

--- Tests/Command/GeneratorAwareCommandTest.php
+++ Tests/Command/GeneratorAwareCommandTest.php
@@ -25,7 +25,7 @@ class GeneratorAwareCommandTest extends TestCase
         parent::setUp();
 
         $this->container = $this->getContainer();
-        $this->container->setParameter('propel.path',  __DIR__ . '/../../vendor/propel/propel1');
+        $this->container->setParameter('propel.path',  __DIR__ . '/../../vendor/dayspring-tech/propel1');
     }
 
     public function testGetDatabasesFromSchema()
