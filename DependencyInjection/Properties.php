85af4e80b6a98db1667eb64d52a3d150cedf532e
Jeffrey Wong <jwong@dayspringpartners.com>
2025-02-10 13:21:46 -0800

    fix compat with php8.0

--- DependencyInjection/Properties.php
+++ DependencyInjection/Properties.php
@@ -25,7 +25,7 @@ class Properties
         /**
          * Build properties.
          */
-        private readonly array $properties = []
+        private array $properties = []
     )
     {
     }
